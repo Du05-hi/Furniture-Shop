dd9fb61c41177dcc0a2598f9eda0e026ccf2f449
unknown <duongchidu30072005@gmail.com>
2025-09-12 10:34:37 +0700

    NoiThat

---
+++ FurnitureShop/assets/js/script.js
@@ -0,0 +1,2 @@
+let products=[{id:1,name:"Bàn gỗ",price:1500000,img:"assets/img/ban.jpg"}];
+let cart=[];console.log("JS loaded");
---
+++ SQLQuery1.sql
@@ -0,0 +1,21 @@
+﻿CREATE DATABASE DuongChiDu_QLNoiThat;
+GO
+
+USE DuongChiDu_QLNoiThat;
+GO
+
+CREATE TABLE quanlynoithat (
+    Id INT IDENTITY(1,1) PRIMARY KEY,
+    Name NVARCHAR(100) NOT NULL,
+    Price DECIMAL(18,2) NOT NULL,
+    Description NVARCHAR(255),
+    ImageUrl NVARCHAR(255),
+    Category NVARCHAR(50)
+);
+
+-- Dữ liệu mẫu
+INSERT INTO quanlynoithat (Name, Price, Description, ImageUrl, Category)
+VALUES 
+(N'Sofa gỗ', 5000000, N'Sofa gỗ cao cấp 3 chỗ ngồi', 'sofa.jpg', N'Sofa'),
+(N'Bàn ăn 6 ghế', 7000000, N'Bàn ăn gỗ tự nhiên kèm 6 ghế', 'banan.jpg', N'Bàn'),
+(N'Tủ quần áo 3 cánh', 4500000, N'Tủ quần áo gỗ công nghiệp', 'tuquanao.jpg', N'Tủ');
